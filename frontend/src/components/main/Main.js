32ac89ccf3505b9cea8365e9d6bbc8bccc638f44
Matija <matija@devflection.com>
2022-03-16 21:43:29 +0100

    Extracted the healthcheck to a separate component and parametrized it

--- frontend/src/components/main/Main.js
+++ frontend/src/components/main/Main.js
@@ -1,38 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import Healthcheck from '../healthcheck/Healthcheck';
 import './Main.css';
 
-function Main() {    
-    const [isBackendOnline, setBackendOnline] = useState(false);
-
-    const healthcheck = async () => {
-        const healthcheckResult = await fetch("backend:8082/health")
-        .then(resp => resp.ok)
-        .catch(() => false);
-
-        setBackendOnline(healthcheckResult);
-    }
-
-    useEffect(() => {        
-        let checkBackendInterval = setInterval(() => {
-            healthcheck()
-        }, 15000);
-
-        return () => clearInterval(checkBackendInterval);
-    }, [isBackendOnline]);
-
-    return (
-        <div className={`main-container ${isBackendOnline ? 'online' : 'offline'}`}>
-            {getMessage(isBackendOnline)}
+function Main() {
+    return (        
+        <div className='main-container'>            
+            <Healthcheck serviceUrl="backend:8080/health" serviceName="Backend" />
+            <Healthcheck serviceUrl="redis:6379/health" serviceName="Redis" />
+            <Healthcheck serviceUrl="mongo:27017/health" serviceName="Mongo" />            
         </div>
     );
-
-    function getMessage(isBackendOnline) {
-        if (isBackendOnline) {
-            return <div className='message'>Backend is online!</div>;
-        } else {
-            return <div className='message'>Backend is offline!</div>;
-        }
-    }
 }
 
 export default Main;
