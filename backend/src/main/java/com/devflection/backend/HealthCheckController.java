0ddabc4c233cf78590b74d0eecce980ffffedaae
Matija <matija@devflection.com>
2022-03-17 22:54:32 +0100

    Changed CORS policy to allow all origins

--- backend/src/main/java/com/devflection/backend/HealthCheckController.java
+++ backend/src/main/java/com/devflection/backend/HealthCheckController.java
@@ -11,7 +11,7 @@ class HealthCheckController {
 
     @GetMapping("/health")
     @ResponseStatus(HttpStatus.OK)
-    @CrossOrigin(origins = "http://localhost:3000")
+    @CrossOrigin(origins = "*")
     String healthcheck() {
         return "ok";
     }
