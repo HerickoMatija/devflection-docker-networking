7e7b0314f766d13673e14e902702ecb8cb8e86a9
Matija <matija@devflection.com>
2022-03-24 21:56:02 +0100

    Added docker and docker-compose files to celtics folder

--- celtics/hr/src/main/java/com/devflection/celtics/services/hr/HumanResourcesController.java
+++ celtics/hr/src/main/java/com/devflection/celtics/services/hr/HumanResourcesController.java
@@ -1,31 +1,19 @@
 package com.devflection.celtics.services.hr;
 
-import java.util.Map;
-
-import javax.money.CurrencyUnit;
-import javax.money.Monetary;
-
-import org.javamoney.moneta.Money;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController()
 public class HumanResourcesController {
     
-    private static final CurrencyUnit USD = Monetary.getCurrency("USD");
-
-    @GetMapping("/availableToTrade")
+    @GetMapping("/hr")
     @ResponseStatus(HttpStatus.OK)
     @CrossOrigin(origins = "*")
-    public Map<String, Money> health() {
-        return Map.of(
-            "Al Horford", Money.of(27000000, USD),
-            "Marcus Smart", Money.of(14000000, USD),
-            "Jabari Parker", Money.of(1100000, USD)
-        );
+    public String health() {
+        return "Hello from the Celtics HR Department!";
     }
 
 }
